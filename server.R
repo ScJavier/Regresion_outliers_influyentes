0b6cda4f5c83df50409d2bb147c65fcb69f41a8d
ScJavier <jsantibanezc@yahoo.com>
2017-11-20 22:50:00 -0600

    Multiple regression complete

--- server.R
+++ server.R
@@ -1,8 +1,4 @@
-options(rgl.useNULL=T)
-
 library(shiny)
-library(shinyRGL)
-library(rgl)
 
 server <- function(input, output) {
   
@@ -42,79 +38,82 @@ server <- function(input, output) {
     par(mar = c(4.1, 4.1, 1, 1))
     plot(datos[-1,], pch = 16, col = 'darkcyan',
          xlim = limites_x, ylim = limites_y,
-         xlab = 'Variable explicativa (X)', ylab = 'Variable respuesta (Y)')
-    points(datos[1,], pch = 16, col = 'red3')
+         xlab = 'Variable explicativa (X)', ylab = 'Variable respuesta (Y)', cex = 2)
+    points(datos[1,], pch = 16, col = 'red3', cex = 2)
     abline(lm(y ~ x, datos), col = 'red3', lwd = 3)
     abline(lm(y ~ x, datos[-1,]), col = 'darkcyan', lwd = 3)
   })  
+  
+  
+  output$outliers_simple <- renderPlot({
+    datos <- actualiza_datos_simple()
+    
+    modelo <- lm(y ~ x, datos)
+    ajustados <- fitted(modelo)
+    
+    library(MASS)
+    residuos <- studres(modelo)
+    
+    par(mar = c(4.1, 4.1, 1, 1))
+    plot(ajustados, residuos, pch = 16, col = c('red3', rep('darkcyan', input$n_simple-1)),
+         xlab = 'Valores ajustados', ylab = 'Residuos studentizados', cex = 2)
+    abline(h = c(-3, 3), col = 'red3', lwd = 3)
+  })
 
-  # Estadísticos para observaciones atípicas de la regresión simple
+  # Estadísticos para observaciones influyentes de la regresión simple
   ## Leverage
   output$leverage_simple <- renderPrint({
     datos <- actualiza_datos_simple()
     leverage <- hatvalues(lm(y ~ x, datos))[1]
-    punto_corte <- 2/input$n_simple
-    
-    auxiliar <- c(leverage, punto_corte)
-    names(auxiliar) <- c('Leverage(i)', 'Punto de Corte')
+    auxiliar <- c(leverage)
+    names(auxiliar) <- c('Leverage(i)')
     round(auxiliar, 5)
   })
   
   ## DFBETAS
   output$dfbetas_simple <- renderPrint({
     datos <- actualiza_datos_simple()
-    punto_corte <- 2/sqrt(input$n_simple)
-    auxiliar <- c(dfbetas(lm(y ~ x, datos))[1,], punto_corte)
-    names(auxiliar) <- c('DFBETAS(i,0)', 'DFBETAS(i,1)', 'Punto de corte')
+    auxiliar <- c(dfbetas(lm(y ~ x, datos))[1,])
+    names(auxiliar) <- c('DFBETAS(i,0)', 'DFBETAS(i,1)')
     round(auxiliar, 5)
   })
   
   ## DFFITS
   output$dffits_simple <- renderPrint({
     datos <- actualiza_datos_simple()
-    punto_corte <- 2/sqrt(input$n_simple)
-    auxiliar <- c(dffits(lm(y ~ x, datos))[1], punto_corte)
-    names(auxiliar) <- c('DFFITS(i)', 'Punto de corte')
+    auxiliar <- c(dffits(lm(y ~ x, datos))[1])
+    names(auxiliar) <- c('DFFITS(i)')
     round(auxiliar, 5)
   })
   
   ## COVRATIO
   output$covratio_simple <- renderPrint({
     datos <- actualiza_datos_simple()
-    puntos_corte <- 1 + c(-1, 1)*3/input$n_simple
-    auxiliar <- c(covratio(lm(y ~ x, datos))[1], puntos_corte)
-    names(auxiliar) <- c('COVRATIO(i)', 'PC inferior', 'PC superior')
+    auxiliar <- c(covratio(lm(y ~ x, datos))[1])
+    names(auxiliar) <- c('COVRATIO(i)')
     round(auxiliar, 5)
   })
   
-  # Gráfica de dispersión de la regresión múltiple
-  output$prueba <- renderRglwidget({
+  output$dispersion_multiple <- renderPlot({
     datos <- actualiza_datos_multiple()
     
-    plot3d(x = datos[,1], y = datos[,2], z = datos[,3],
-           col = c('red3', rep('darkcyan', 999)),
-           size = 1, type = 's', box = F,
-           xlab = 'X1', ylab = 'X2', zlab = 'Y')
-
     modelo_completo <- lm(y ~ x1 + x2, data = datos)
     modelo_incompleto <- lm(y ~ x1 + x2, data = datos[-1,])
     
-    predichos_completo <- expand.grid(x1 = seq(min(datos$x1), max(datos$x1), length.out = 100),
-                                      x2 = seq(min(datos$x2), max(datos$x2), length.out = 100))
-    predichos_completo$y <- predict(modelo_completo, predichos_completo)
+    x <- seq(min(datos$x1), max(datos$x1), length.out = 25)
+    y <- seq(min(datos$x2), max(datos$x2), length.out = 25)
     
-    predichos_incompleto <- expand.grid(x1 = seq(min(datos$x1), max(datos$x1), length.out = 100),
-                                        x2 = seq(min(datos$x2), max(datos$x2), length.out = 100))
-    predichos_incompleto$y <- predict(modelo_incompleto, predichos_incompleto)
+    z1 <- outer(x, y, function(x, y) predict(modelo_completo, data.frame(x1 = x, x2 = y)))
+    z2 <- outer(x, y, function(x, y) predict(modelo_incompleto, data.frame(x1 = x, x2 = y)))
     
-    surface3d(unique(predichos_completo[,1]),
-              unique(predichos_completo[,2]),
-              predichos_completo[,3], col = 'red3', alpha = 0.5)
-    
-    surface3d(unique(predichos_incompleto[,1]),
-              unique(predichos_incompleto[,2]),
-              predichos_incompleto[,3], col = 'darkcyan', alpha = 0.5)
-    rglwidget()
+    plano_completo <- persp(x, y, z1, theta = input$theta, phi = input$phi, col = "red3",
+                            shade = 0.5, ticktype = "detailed", ltheta = 30, lphi = 15)
+    par(new = TRUE)
+    plano_incompleto <- persp(x, y, z2, theta = input$theta, phi = input$phi, col = "darkcyan",
+                              shade = NA, ticktype = "simple", ltheta = 30, lphi = 15, axes = F)
+    puntos_completo <- trans3d(datos[,1], datos[,2], datos[,3], plano_completo)
+    points(puntos_completo, col = c('red3', rep('darkcyan', input$n_multiple-1)),
+           pch = 16, cex = 2)
   })
   
   
@@ -123,37 +122,32 @@ server <- function(input, output) {
   output$leverage_multiple <- renderPrint({
     datos <- actualiza_datos_multiple()
     leverage <- hatvalues(lm(y ~ x1 + x2, datos))[1]
-    punto_corte <- 4/input$n_multiple
-    
-    auxiliar <- c(leverage, punto_corte)
-    names(auxiliar) <- c('Leverage(i)', 'Punto de Corte')
+    auxiliar <- c(leverage)
+    names(auxiliar) <- c('Leverage(i)')
     round(auxiliar, 5)
   })
   
   ## DFBETAS
   output$dfbetas_multiple <- renderPrint({
     datos <- actualiza_datos_multiple()
-    punto_corte <- 2/sqrt(input$n_multiple)
-    auxiliar <- c(dfbetas(lm(y ~ x1 + x2, datos))[1,], punto_corte)
-    names(auxiliar) <- c('DFBETAS(i,0)', 'DFBETAS(i,1)', 'DFBETAS(i,3)', 'Punto de corte')
+    auxiliar <- c(dfbetas(lm(y ~ x1 + x2, datos))[1,])
+    names(auxiliar) <- c('DFBETAS(i,0)', 'DFBETAS(i,1)', 'DFBETAS(i,3)')
     round(auxiliar, 5)
   })
   
   ## DFFITS
   output$dffits_multiple <- renderPrint({
     datos <- actualiza_datos_multiple()
-    punto_corte <- 2/sqrt(2/input$n_multiple)
-    auxiliar <- c(dffits(lm(y ~ x1 + x2, datos))[1], punto_corte)
-    names(auxiliar) <- c('DFFITS(i)', 'Punto de corte')
+    auxiliar <- c(dffits(lm(y ~ x1 + x2, datos))[1])
+    names(auxiliar) <- c('DFFITS(i)')
     round(auxiliar, 5)
   })
   
   ## COVRATIO
   output$covratio_multiple <- renderPrint({
     datos <- actualiza_datos_multiple()
-    puntos_corte <- 1 + c(-1, 1)*6/input$n_multiple
-    auxiliar <- c(covratio(lm(y ~ x1 + x2, datos))[1], puntos_corte)
-    names(auxiliar) <- c('COVRATIO(i)', 'PC inferior', 'PC superior')
+    auxiliar <- c(covratio(lm(y ~ x1 + x2, datos))[1])
+    names(auxiliar) <- c('COVRATIO(i)')
     round(auxiliar, 5)
   })
 }
--- ui.R
+++ ui.R
@@ -1,60 +1,59 @@
-
 library(shiny)
-library(shinyRGL)
-library(rgl)
 
 ui <- navbarPage("Observaciones atípicas e influyentes", fluid = T,
                  tabPanel("Regresión simple",
                           fluidRow(
-                            column(3,
+                            column(2,
                               wellPanel(
-                                numericInput('n_simple', 'Tamaño de muestra', value = 30, step = 10, min = 10, max = 1000),
-                                numericInput('x_simple', 'Variable explicativa (X)', value = 50, step = 1),
-                                numericInput('y_simple', 'Variable respuesta (Y)', value = 275, step = 5)
+                                h3('Parámetros'),
+                                numericInput('n_simple', 'n', value = 30, step = 10, min = 10, max = 1000),
+                                numericInput('x_simple', 'X', value = 50, step = 1),
+                                numericInput('y_simple', 'Y', value = 275, step = 5)
                               )
                             ),
-                            column(8, offset = 1, 
-                              plotOutput('dispersion_simple')
-                            )
-                          ),
-                          br(),
-                          fluidRow(
-                            column(4, offset = 4,
-                              h4('Leverage'),
-                              verbatimTextOutput('leverage_simple'),
-                              h4('DFBETAS'),
-                              verbatimTextOutput('dfbetas_simple'),
-                              h4('DFFITS'),
-                              verbatimTextOutput('dffits_simple'),
-                              h4('COVRATIO'),
-                              verbatimTextOutput('covratio_simple')
+                            column(6, plotOutput('dispersion_simple')),
+                            column(4,
+                                   h4('Leverage'),
+                                   verbatimTextOutput('leverage_simple'),
+                                   h4('DFBETAS'),
+                                   verbatimTextOutput('dfbetas_simple'),
+                                   h4('DFFITS'),
+                                   verbatimTextOutput('dffits_simple'),
+                                   h4('COVRATIO'),
+                                   verbatimTextOutput('covratio_simple')
                             )
                           )
                  ),
                  
                  tabPanel("Regresión múltiple",
                           fluidRow(
-                            column(3,
+                            column(2,
                                    wellPanel(
-                                     numericInput('n_multiple', 'Tamaño de muestra', value = 30, step = 10, min = 10, max = 1000),
-                                     numericInput('x1_multiple', 'Variable explicativa 1 (X1)', value = 30, step = 5),
-                                     numericInput('x2_multiple', 'Variable explicativa 2 (X2)', value = 50, step = 10),
-                                     numericInput('y_multiple', 'Variable respuesta (Y)', value = 420, step = 25)
+                                     h3('Parámetros'),
+                                     numericInput('n_multiple', 'n', value = 30, step = 10, min = 10, max = 1000),
+                                     numericInput('x1_multiple', 'X1', value = 30, step = 5),
+                                     numericInput('x2_multiple', 'X2', value = 50, step = 10),
+                                     numericInput('y_multiple', 'Y', value = 420, step = 25)
                                 )
                             ),
-                            column(8,
-                                   rglwidgetOutput("prueba", width = '900px', height = '600px')
+                            column(6, plotOutput('dispersion_multiple')),
+                            column(4,
+                                   h4('Leverage'),
+                                   verbatimTextOutput('leverage_multiple'),
+                                   h4('DFBETAS'),
+                                   verbatimTextOutput('dfbetas_multiple'),
+                                   h4('DFFITS'),
+                                   verbatimTextOutput('dffits_multiple'),
+                                   h4('COVRATIO'),
+                                   verbatimTextOutput('covratio_multiple')
                             )
                           ),
                           br(),
                           fluidRow(
-                            column(4, offset = 4,
-                                   h4('Leverage'),
-                                   verbatimTextOutput('leverage_multiple'),
-                                   h4('DFBETAS'),
-                                   h4('DFFITS'),
-                                   h4('COVRATIO')
-                            )
+                            column(4, offset = 2,
+                                   sliderInput('theta', 'Azimut', value = 30, step = 1, min = 0, max = 360,
+                                               animate = animationOptions(interval = 100, loop = TRUE))),
+                            column(4, sliderInput('phi', 'Colatitud', value = 20, step = 20, min = -40, max = 40))
                           )
                  )
 )
